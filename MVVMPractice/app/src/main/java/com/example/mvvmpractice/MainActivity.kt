41d9ff2bc51fb68aed1ab7be7e07768786dc1154
junho <kimjjun1007@gmail.com>
2021-12-13 15:06:36 +0900

    add::viewBinding

--- MVVMPractice/app/src/main/java/com/example/mvvmpractice/MainActivity.kt
+++ MVVMPractice/app/src/main/java/com/example/mvvmpractice/MainActivity.kt
@@ -2,12 +2,29 @@ package com.example.mvvmpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvmpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding : ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mBinding!!.text.text = "token"
+
+        mBinding!!.button.setOnClickListener {
+            mBinding!!.text.text = "ㅅㅂ"
+        }
+    }
+
+    override fun onDestroy() {
+        mBinding = null
+        super.onDestroy()
     }
 }
